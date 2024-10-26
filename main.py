b5de63598f7a34b8fbfa98e66648dcc7abcf78b2
Pavel Nikolaev <panikolaev7@gmail.com>
2024-10-26 12:05:34 +0300

    Added interface, add def show_coordinates

--- main.py
+++ main.py
@@ -7,17 +7,37 @@ from opencage.geocoder import OpenCageGeocode
 def get_coordinates(city, key):
     try:
         geocoder = OpenCageGeocode(key)
-        query = city
-        results = geocoder.geocode(query)
+        results = geocoder.geocode(city, language="ru")
         if results:
-            return results[0]['geometry']['lat'], results[0]['geometry']['lng']
+            lat = round(results[0]['geometry']['lat'], 2)
+            lng = round(results[0]['geometry']['lng'], 2)
+            return f"Широта: {lat}, Долгота {lng}"
         else:
             return "Город не найден"
     except Exception as exc:
         mb.showerror("Ошибка!", f"Произошла ошибка: {exc}!")
 
 
+def show_coordinates():
+    city = entry.get()
+    coordinates = get_coordinates(city, key)
+    label.config(text=f"Координаты города {city}:\n{coordinates}")
+
+
 key = '6686f5227c304d129ab326a8ffd9a887'
-city = "Москва"
-coordinates = get_coordinates(city, key)
-print(f"Координаты города {city}: {coordinates}")
+
+
+window = Tk()
+window.title("Координаты городов")
+window.geometry("200x150")
+
+entry = ttk.Entry(window)
+entry.pack(pady=10)
+
+button = ttk.Button(window, text="Поиск координат", command=show_coordinates)
+button.pack(pady=(0, 10))
+
+label = ttk.Label(text="Введите город и нажмите кнопку")
+label.pack(pady=(0, 10))
+
+window.mainloop()
